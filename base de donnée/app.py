2d07504491fdfed190d2fcc120554214b2f6752c
Word.exe <welhadji@francoisdesales.be>
2024-02-12 19:12:17 +0100

    Update app.py
    /

--- base de donnée/app.py
+++ base de donnée/app.py
@@ -2,7 +2,16 @@ import sqlite3
 
 connexion = sqlite3.connect("BaseDonnée.db")
 cursor = connexion.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS character (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, )") #add fk
-cursor.execute("CREATE TABLE IF NOT EXISTS campagne (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, horodatage DATETIME)") #add fk
-cursor.execute("CREATE TABLE IF NOT EXISTS inscription (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, horodatage DATETIME)") #add fk
-cursor.execute("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, username TEXT, email TEXT, password TEXT)") #add fk
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS character (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, )"
+)  # add fk
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS campagne (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, horodatage DATETIME)"
+)  # add fk
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS inscription (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, horodatage DATETIME)"
+)  # add fk
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, username TEXT, email TEXT, password TEXT)"
+)  # add fk
+print("HELLO WORLD NIIGER")
