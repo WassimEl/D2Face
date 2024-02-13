b2d93ca1f06fb88e8e71f47dfce85effdd052387
Word.exe <welhadji@francoisdesales.be>
2024-02-13 19:32:28 +0100

    app update
    set link with sql scripts and creation of cursor to connect to the db

--- database/app.py
+++ database/app.py
@@ -1,7 +1,58 @@
 import sqlite3
+import os
 
 connexion = sqlite3.connect("database.db")
-cursor = connexion.cursor()
+
+
+#
+def get_db_n():
+    file_path = os.path.realpath(__file__)
+    work_dir = os.path.dirname(file_path)
+    db_name = f"{work_dir}/data.db"
+    return db_name
+
+
+#
+def get_sql_s():
+    file_path = os.path.realpath(__file__)
+    work_dir = os.path.dirname(file_path)
+    sql_init = f"{work_dir}/sql-scripts/init-horaire-train.sql"
+    return sql_init
+
+
+#
+def fn_init_db(db_name, sql_init):
+    sqliteConnection = None
+    try:
+        with sqlite3.connect(db_name, timeout=10) as sqliteConnection:
+            print(f"Connected to the {db_name}")
+            cursor = sqliteConnection.cursor()
+            try:
+                with open(sql_init, "r") as sqlite_file:
+                    try:
+                        sql_script = sqlite_file.read()
+                    except Exception as error:
+                        print(f"Error while reading the SQL script: {error}")
+                        return
+            except Exception as error:
+                print(f"Error while opening the SQL file: {error}")
+                return
+            try:
+                cursor.executescript(sql_script)
+                print("SQLite script executed successfully")
+            except sqlite3.Error as error:
+                print(f"Error while executing SQLite script: {error}")
+            finally:
+                cursor.close()
+    except sqlite3.Error as error:
+        print(f"Error while connecting to SQLite: {error}")
+    except Exception as error:
+        print(f"{error}")
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
 
 connexion.commit()
 
