f25ae75220a995cbcd37716209538447ed2fd476
Word.exe <welhadji@francoisdesales.be>
2024-03-27 01:21:15 +0100

    rework form of code
    adding comments
    
    bug fix "displaying characters of a user"
    bug fix "creating character from user id "

---
+++ Models.py
@@ -0,0 +1,18 @@
+def fninit_menu():
+    q_choix_1 = "[1] Créer une fiche de personnage"
+    q_choix_2 = "[2] Supprimer une fiche"
+    q_choix_3 = "[3] Afficher les fiches"
+    q_choix_4 = "[4] Quitter"
+    list_menu = [q_choix_1, q_choix_2, q_choix_3, q_choix_4]
+    return list_menu
+
+
+def fn_init_menu():
+    q_choix_1 = "[1] Créer une base de données"
+    q_choix_2 = "[2] Encoder des données"
+    q_choix_3 = "[3] Afficher des données"
+    q_choix_4 = "[4] Mettre-à-jour des données"
+    q_choix_5 = "[5] Supprimer des données"
+    q_choix_6 = "[6] Quitter"
+    list_menu = [q_choix_1, q_choix_2, q_choix_3, q_choix_4, q_choix_5, q_choix_6]
+    return list_menu
